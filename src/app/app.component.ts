78beb16d92a2e8734af8fc6c19fa3921cdb0c108
sagar <sagar@stackmint.com>
2020-02-21 12:42:38 +0530

    generalised format created for graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,28 @@ am4core.useTheme(am4themes_animated);
 })
 export class AppComponent {
 
-  private chart: am4charts.XYChart;
-  // private pieChart: am4charts.PieChart;
+  /* chart.data = [
+      {
+        "CIPCircuitName": "TestCircuit1",
+        "Caustic": 36,
+        "Acid": 15,
+        "Caustic Acid": 8
+      },
+      {
+        "CIPCircuitName": "TestCircuit2",
+        "Caustic": 21,
+        "Acid": 15,
+        "Caustic Acid": 55
+      },
+      {
+        "CIPCircuitName": "TestCircuit3",
+        "Caustic": 5,
+        "Acid": 45,
+        "Caustic Acid": 29
+      }
+  ]; */
+
+  private stackedChart: am4charts.XYChart;
   private lineChart: am4charts.XYChart;
 
   constructor(
@@ -23,37 +43,55 @@ export class AppComponent {
   ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
     this.getMockData();
   }
 
   getMockData() {
     this.dataService.getMockData().subscribe(
       resp => {
-        this.formatAPIDataForStackChart(resp.Data, "CIPCircuitName", "CIPProgram");
+
+        let xAxisHeader = "CIPCircuitName";
+        let xAxisHeaderAlias = "CIP Circuit Name";
+
+        let yAxisHeader = "CIPProgram";
+        let yAxisHeaderAlias = "CIP Program";
+
+        this.formatAPIDataForStackChart(resp.Data, xAxisHeader, xAxisHeaderAlias, yAxisHeader, yAxisHeaderAlias);
+        this.formatAPIDataForLineChart(resp.Data, xAxisHeader, xAxisHeaderAlias, yAxisHeader, yAxisHeaderAlias);
+
       }
     );
   }
 
-  formatAPIDataForStackChart(apiData, xAxis: string, yAxis: string) {
+  formatAPIDataForStackChart(
+    apiData,
+    xAxisHeader: string,
+    xAxisHeaderAlias: string,
+    yAxisHeader: string,
+    yAxisHeaderAlias: string
+  ) {
 
-    let uniqueXAxis = [...new Set(apiData.map(item => item[xAxis]))];
-    let uniqueYAxis = [...new Set(apiData.map(item => item[yAxis]))];
+    let uniqueXAxis = [...new Set(apiData.map(item => item[xAxisHeader]))];
+    let uniqueYAxis = [...new Set(apiData.map(item => item[yAxisHeader]))];
 
     let dataForStackChart = [];
 
-    for (let index = 0; index < uniqueXAxis.length; index++) {
+    for (let i = 0; i < uniqueXAxis.length; i++) {
       dataForStackChart.push({});
-      dataForStackChart[index][xAxis] = uniqueXAxis[index];
-      for (let index1 = 0; index1 < uniqueYAxis.length; index1++) {
-        dataForStackChart[index][(uniqueYAxis[index1]).toString()] = 0;
+      dataForStackChart[i][xAxisHeader] = uniqueXAxis[i];
+      for (let j = 0; j < uniqueYAxis.length; j++) {
+        dataForStackChart[i][(uniqueYAxis[j]).toString()] = 0;
       }
     }
 
     for (let crrApiData of apiData) {
       for (let crrDataForStackChart of dataForStackChart) {
-        if (crrDataForStackChart[xAxis] == crrApiData[xAxis]) { //CIPCircuitName matches
+        if (crrDataForStackChart[xAxisHeader] == crrApiData[xAxisHeader]) { //CIPCircuitName matches
           for (let yAxisValue of uniqueYAxis) {
-            if (crrApiData[yAxis] == yAxisValue) { //CIPProgramName matches
+            if (crrApiData[yAxisHeader] == yAxisValue) { //CIPProgramName matches
               crrDataForStackChart[yAxisValue.toString()] = crrDataForStackChart[yAxisValue.toString()] + 1;
             }
           }
@@ -61,176 +99,207 @@ export class AppComponent {
       }
     }
 
-    console.log('dataForStackChart', dataForStackChart);
-
-    this.chart.data = dataForStackChart;
-    this.lineChart.data = dataForStackChart;
+    this.renderStackChart(
+      dataForStackChart,
+      xAxisHeader,
+      xAxisHeaderAlias,
+      yAxisHeader,
+      yAxisHeaderAlias,
+      uniqueYAxis
+    );
 
   }
 
-  ngAfterViewInit() {
+  formatAPIDataForLineChart(
+    apiData,
+    xAxisHeader: string,
+    xAxisHeaderAlias: string,
+    yAxisHeader: string,
+    yAxisHeaderAlias: string
+  ) {
+
+    let uniqueXAxis = [...new Set(apiData.map(item => item[xAxisHeader]))];
+    let uniqueYAxis = [...new Set(apiData.map(item => item[yAxisHeader]))];
+
+    let dataForLineChart = [];
+
+    for (let i = 0; i < uniqueXAxis.length; i++) {
+      dataForLineChart.push({});
+      dataForLineChart[i][xAxisHeader] = uniqueXAxis[i];
+      for (let j = 0; j < uniqueYAxis.length; j++) {
+        dataForLineChart[i][(uniqueYAxis[j]).toString()] = 0;
+      }
+    }
+
+    for (let crrApiData of apiData) {
+      for (let crrDataForStackChart of dataForLineChart) {
+        if (crrDataForStackChart[xAxisHeader] == crrApiData[xAxisHeader]) { //CIPCircuitName matches
+          for (let yAxisValue of uniqueYAxis) {
+            if (crrApiData[yAxisHeader] == yAxisValue) { //CIPProgramName matches
+              crrDataForStackChart[yAxisValue.toString()] = crrDataForStackChart[yAxisValue.toString()] + 1;
+            }
+          }
+        }
+      }
+    }
+
+    this.renderLineChart(
+      dataForLineChart,
+      xAxisHeader,
+      xAxisHeaderAlias,
+      yAxisHeader,
+      yAxisHeaderAlias,
+      uniqueYAxis
+    );
+
+  }
+
+  renderStackChart(
+    dataForStackChart,
+    xAxisHeader: string,
+    xAxisHeaderAlias: string,
+    yAxisHeader: string,
+    yAxisHeaderAlias: string,
+    uniqueYAxis
+  ) {
+
     this.zone.runOutsideAngular(() => {
 
-      let chart = am4core.create("chartdiv", am4charts.XYChart);
+      let stackedChart = am4core.create("chartdiv", am4charts.XYChart);
 
-    /*   chart.data = [
-        {
-          "CIPCircuitName": "TestCircuit1",
-          "Caustic": 36,
-          "Acid": 15,
-          "Caustic Acid": 8
-        },
-        {
-          "CIPCircuitName": "TestCircuit2",
-          "Caustic": 21,
-          "Acid": 15,
-          "Caustic Acid": 55
-        },
-        {
-          "CIPCircuitName": "TestCircuit3",
-          "Caustic": 5,
-          "Acid": 45,
-          "Caustic Acid": 29
-        }
-      ]; */
+      stackedChart.data = dataForStackChart;
 
-      // Create axes
-      var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-      categoryAxis.dataFields.category = "CIPCircuitName";
-      categoryAxis.title.text = "CIP Circuit Name";
+      var categoryAxis = stackedChart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = xAxisHeader;
+      categoryAxis.title.text = xAxisHeaderAlias;
       categoryAxis.renderer.grid.template.location = 0;
       categoryAxis.renderer.minGridDistance = 20;
 
-
-      var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      valueAxis.title.text = "CIP Counts";
+      var valueAxis = stackedChart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.title.text = yAxisHeaderAlias;
       valueAxis.calculateTotals = true;
       valueAxis.min = 0;
 
-      // Create series
-      var series = chart.series.push(new am4charts.ColumnSeries());
-      series.dataFields.valueY = "Caustic";
-      // series.dataFields.valueYShow = "total";
-      series.dataFields.categoryX = "CIPCircuitName";
-      series.name = "Caustic";
-      series.tooltipText = "{name}: [bold]{valueY}[/]";
-      series.stacked = true;
-
-      var series2 = chart.series.push(new am4charts.ColumnSeries());
-      series2.dataFields.valueY = "Acid";
-      // series2.dataFields.valueYShow = "total";
-      series2.dataFields.categoryX = "CIPCircuitName";
-      series2.name = "Acid";
-      series2.tooltipText = "{name}: [bold]{valueY}[/]";
-      series2.stacked = true;
-
-      var series3 = chart.series.push(new am4charts.ColumnSeries());
-      series3.dataFields.valueY = "Caustic Acid";
-      // series3.dataFields.valueYShow = "total";
-      // series3.fill = am4core.color('#189b1b');
-      // series3.stroke = am4core.color('#189b1b');
-      series3.dataFields.categoryX = "CIPCircuitName";
-      series3.name = "Caustic Acid";
-      series3.tooltipText = "{name}: [bold]{valueY}[/]";
-      series3.stacked = true;
-
-      // Add cursor
-      chart.cursor = new am4charts.XYCursor();
-      this.chart = chart;
-
-
-      //----------------------Line Chart--------------------------//
-
-      let lineChart = am4core.create("chartdiv1", am4charts.XYChart);
-
-      // Create axes
-      var categoryAxis = lineChart.xAxes.push(new am4charts.CategoryAxis());
-      categoryAxis.dataFields.category = "CIPCircuitName";
-      categoryAxis.title.text = "CIP Circuit Name";
-      categoryAxis.renderer.grid.template.location = 0;
-      categoryAxis.renderer.minGridDistance = 20;
-
-      // Create series
-      function createAxisAndSeries(field, name, opposite, bullet) {
-        let valueAxis = lineChart.yAxes.push(new am4charts.ValueAxis());
-        /*  if (lineChart.yAxes.indexOf(valueAxis) != 0) {
-           valueAxis.syncWithAxis = lineChart.yAxes.getIndex(0) as any;
-         } */
-
-        valueAxis.title.text = "CIP Counts";
-        valueAxis.calculateTotals = true;
-        valueAxis.min = 0;
-
-        let series = lineChart.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = field;
-        series.dataFields.categoryX = "CIPCircuitName";
-        series.strokeWidth = 2;
-        series.yAxis = valueAxis;
-        series.name = name;
-        series.tooltipText = "{name}: [bold]{valueY}[/]";
-        series.tensionX = 0.8;
-        series.showOnInit = true;
-
-        let interfaceColors = new am4core.InterfaceColorSet();
-
-        switch (bullet) {
-          case "triangle":
-            let bullet1 = series.bullets.push(new am4charts.Bullet());
-            bullet1.width = 12;
-            bullet1.height = 12;
-            bullet1.horizontalCenter = "middle";
-            bullet1.verticalCenter = "middle";
-
-            let triangle = bullet1.createChild(am4core.Triangle);
-            triangle.stroke = interfaceColors.getFor("background");
-            triangle.strokeWidth = 2;
-            triangle.direction = "top";
-            triangle.width = 12;
-            triangle.height = 12;
-            break;
-          case "rectangle":
-            let bullet2 = series.bullets.push(new am4charts.Bullet());
-            bullet2.width = 10;
-            bullet2.height = 10;
-            bullet2.horizontalCenter = "middle";
-            bullet2.verticalCenter = "middle";
-
-            let rectangle = bullet2.createChild(am4core.Rectangle);
-            rectangle.stroke = interfaceColors.getFor("background");
-            rectangle.strokeWidth = 2;
-            rectangle.width = 10;
-            rectangle.height = 10;
-            break;
-          default:
-            let bullet3 = series.bullets.push(new am4charts.CircleBullet());
-            bullet3.circle.stroke = interfaceColors.getFor("background");
-            bullet3.circle.strokeWidth = 2;
-            break;
-        }
-
-        valueAxis.renderer.line.strokeOpacity = 1;
-        valueAxis.renderer.line.strokeWidth = 2;
-        valueAxis.renderer.line.stroke = series.stroke;
-        valueAxis.renderer.labels.template.fill = series.stroke;
-        valueAxis.renderer.opposite = opposite;
+      let arrSeriesVariables = [];
+      for (let k = 0; k < uniqueYAxis.length; k++) {
+        arrSeriesVariables.push(stackedChart.series.push(new am4charts.ColumnSeries()));
+        arrSeriesVariables[k].dataFields.valueY = uniqueYAxis[k];
+        arrSeriesVariables[k].dataFields.categoryX = xAxisHeader;
+        arrSeriesVariables[k].name = uniqueYAxis[k];
+        arrSeriesVariables[k].tooltipText = "{name}: [bold]{valueY}[/]";
+        arrSeriesVariables[k].stacked = true;
       }
 
-      createAxisAndSeries("Caustic", "Caustic", false, "circle");
-      createAxisAndSeries("Acid", "Acid", true, "triangle");
-      createAxisAndSeries("Caustic Acid", "Caustic Acid", true, "rectangle");
-
-      lineChart.cursor = new am4charts.XYCursor();
-
-      this.lineChart = lineChart;
+      stackedChart.cursor = new am4charts.XYCursor();
+      this.stackedChart = stackedChart;
 
     });
   }
 
+  renderLineChart(
+    dataForLineChart,
+    xAxisHeader: string,
+    xAxisHeaderAlias: string,
+    yAxisHeader: string,
+    yAxisHeaderAlias: string,
+    uniqueYAxis
+  ) {
+
+    let lineChart = am4core.create("chartdiv1", am4charts.XYChart);
+
+    lineChart.data = dataForLineChart
+
+    var categoryAxis = lineChart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = xAxisHeader;
+    categoryAxis.title.text = xAxisHeaderAlias;
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 20;
+
+    for (let l = 0; l < uniqueYAxis.length; l++) {
+      this.createAxisAndSeries(
+        lineChart,
+        yAxisHeaderAlias,
+        xAxisHeader,
+        uniqueYAxis[l],
+        uniqueYAxis[l],
+        (l == 0 ? false : true),
+        "circle"
+      );
+    }
+
+    lineChart.cursor = new am4charts.XYCursor();
+    this.lineChart = lineChart;
+
+  }
+
+  createAxisAndSeries(lineChart, yAxisHeaderAlias, xAxisHeader, field, name, opposite, bullet) {
+
+    let valueAxis = lineChart.yAxes.push(new am4charts.ValueAxis());
+
+    valueAxis.title.text = yAxisHeaderAlias;
+    valueAxis.calculateTotals = true;
+    valueAxis.min = 0;
+
+    let series = lineChart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = field;
+    series.dataFields.categoryX = xAxisHeader;
+    series.strokeWidth = 2;
+    // series.yAxisHeader = valueAxis;
+    series.name = name;
+    series.tooltipText = "{name}: [bold]{valueY}[/]";
+    series.tensionX = 0.8;
+    series.showOnInit = true;
+
+    let interfaceColors = new am4core.InterfaceColorSet();
+
+    switch (bullet) {
+      case "triangle":
+        let bullet1 = series.bullets.push(new am4charts.Bullet());
+        bullet1.width = 12;
+        bullet1.height = 12;
+        bullet1.horizontalCenter = "middle";
+        bullet1.verticalCenter = "middle";
+
+        let triangle = bullet1.createChild(am4core.Triangle);
+        triangle.stroke = interfaceColors.getFor("background");
+        triangle.strokeWidth = 2;
+        triangle.direction = "top";
+        triangle.width = 12;
+        triangle.height = 12;
+        break;
+      case "rectangle":
+        let bullet2 = series.bullets.push(new am4charts.Bullet());
+        bullet2.width = 10;
+        bullet2.height = 10;
+        bullet2.horizontalCenter = "middle";
+        bullet2.verticalCenter = "middle";
+
+        let rectangle = bullet2.createChild(am4core.Rectangle);
+        rectangle.stroke = interfaceColors.getFor("background");
+        rectangle.strokeWidth = 2;
+        rectangle.width = 10;
+        rectangle.height = 10;
+        break;
+      default:
+        let bullet3 = series.bullets.push(new am4charts.CircleBullet());
+        bullet3.circle.stroke = interfaceColors.getFor("background");
+        bullet3.circle.strokeWidth = 2;
+        break;
+    }
+
+    valueAxis.renderer.line.strokeOpacity = 1;
+    valueAxis.renderer.line.strokeWidth = 2;
+    valueAxis.renderer.line.stroke = series.stroke;
+    valueAxis.renderer.labels.template.fill = series.stroke;
+    valueAxis.renderer.opposite = opposite;
+  }
+
   ngOnDestroy() {
     this.zone.runOutsideAngular(() => {
-      if (this.chart) {
-        this.chart.dispose();
-        // this.pieChart.dispose();
+      if (this.stackedChart) {
+        this.stackedChart.dispose();
+      }
+      if (this.lineChart) {
         this.lineChart.dispose();
       }
     });
